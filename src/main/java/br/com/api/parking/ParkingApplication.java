a1c7bdd18813530c113a70817189416296c3c3b8
Caio Ferraz <caioferrazalmeida.27@gmail.com>
2023-12-10 23:30:34 -0300

    feat(config): add security web config

--- src/main/java/br/com/api/parking/ParkingApplication.java
+++ src/main/java/br/com/api/parking/ParkingApplication.java
@@ -2,8 +2,10 @@ package br.com.api.parking;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 @SpringBootApplication
+@CrossOrigin(origins = "*", maxAge = 3600)
 public class ParkingApplication {
 
 	public static void main(String[] args) {
---
+++ src/main/java/br/com/api/parking/config/security/SecurityConfig.java
@@ -0,0 +1,69 @@
+package br.com.api.parking.config.security;
+
+import br.com.api.parking.services.UserDetailServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.ProviderManager;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+  @Bean
+  public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+    http
+            .csrf(AbstractHttpConfigurer::disable)
+            .sessionManagement((session) -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+            .authorizeHttpRequests((authorize) -> authorize
+                    .requestMatchers("auth/register").permitAll()
+                    .requestMatchers("auth/login").permitAll()
+                    .requestMatchers("user").hasRole("ADMIN")
+                    .requestMatchers("api/parking").hasAnyRole("ADMIN", "USER")
+                    .anyRequest().authenticated()
+            );
+
+    return http.build();
+  }
+
+
+  @Bean
+  public AuthenticationManager authenticationManager(
+          UserDetailServiceImpl userDetailsService,
+          PasswordEncoder passwordEncoder) {
+    DaoAuthenticationProvider authenticationProvider = new DaoAuthenticationProvider();
+    authenticationProvider.setUserDetailsService(userDetailsService);
+    authenticationProvider.setPasswordEncoder(passwordEncoder);
+
+    return new ProviderManager(authenticationProvider);
+  }
+
+  @Bean
+  public UserDetailsService userDetailsService() {
+    UserDetails userDetails = User.withDefaultPasswordEncoder()
+            .username("user")
+            .password("password")
+            .roles("USER")
+            .build();
+
+    return new InMemoryUserDetailsManager(userDetails);
+  }
+
+  @Bean
+  public PasswordEncoder passwordEncoder() {
+    return new BCryptPasswordEncoder();
+  }
+
+}
