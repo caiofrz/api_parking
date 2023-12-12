5cca2d1c00b1911ebe6ad9f5c46dcf9f240e9019
Caio Ferraz <caioferrazalmeida.27@gmail.com>
2023-12-11 22:38:03 -0300

    fix(ParkingSpot): modifying uri endpoint path

--- src/main/java/br/com/api/parking/controllers/ParkingSpotController.java
+++ src/main/java/br/com/api/parking/controllers/ParkingSpotController.java
@@ -15,7 +15,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.UUID;
 
 @RestController
-@RequestMapping("api/parking")
+@RequestMapping("spots")
 public class ParkingSpotController {
 
   private final ParkingSpotService service;
